07114337d0d9b1648e4d8fc705086224d20784d7
serkandezcan <serkandezcan@gmail.com>
2025-06-23 21:14:01 +0300

    t1

--- src/MyClass.java
+++ src/MyClass.java
@@ -7,4 +7,7 @@ public class MyClass {
     public String string2 = "Bu da baska bir string'dir";
     //version v3
     public String string3 = "Bu da cok daha baska bir string'dir";
+
+    //Techpro branch, version t1
+    public String string4 = "Bu da Techpro branch'inde olusturulmus bir string'dir";
 }
