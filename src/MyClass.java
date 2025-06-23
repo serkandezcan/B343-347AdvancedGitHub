cf37098750894971260c81d1961f8be9d65a34e9
serkandezcan <serkandezcan@gmail.com>
2025-06-23 22:08:07 +0300

    t3

--- src/MyClass.java
+++ src/MyClass.java
@@ -5,13 +5,19 @@ public class MyClass {
 
     //version v2
     public String string2 = "Bu da baska bir string'dir";
+
     //version v3
     public String string3 = "Bu da cok daha baska bir string'dir";
 
     //Techpro branch, version t1
     public String string4 = "Bu da Techpro branch'inde olusturulmus bir string'dir";
+
     //Techpro branch, version t2
     public String string5 = "Bu da Techpro branch'inde olusturulmus baska bir string'dir";
+
     //version v4
     public String string6 = "Bu artik daha baska bir string'dir";
+
+    //Techpro branch, version t3
+    public String string7 = "Hangi string oldugunun hesabani yapamadim.";
 }
